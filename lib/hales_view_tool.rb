949b207e2e4283bdd8858d77100ac5f06b0e8899
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 14:13:54 +0000

    Dev tool inital commit

---
+++ lib/hales_view_tool.rb
@@ -0,0 +1,6 @@
+require "hales_view_tool/version"
+require "hales_view_tool/renderer"
+
+module HalesViewTool
+  # Your code goes here...
+end
---
+++ lib/hales_view_tool/version.rb
@@ -0,0 +1,3 @@
+module HalesViewTool
+  VERSION = "0.1.0"
+end
